5775616ff2e7003da6ee942192785d9fe55c9df6
Fulya Sariyar <fulyasariyar@outlook.com>
2022-06-09 13:47:31 +0300

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Fulya Sarıyar on 9.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
